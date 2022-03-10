afab9af3260c98862bf09b1336958ae1f4f2f96f
mtsdbe <mtsdbe@gmail.com>
2022-03-10 13:39:06 -0800

    hi

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 int main(void) {
-   puts("\n""Programming is like building a multilingual puzzle");
+   puts("\nProgramming is like building a multilingual puzzle");
    return 0;
 }
