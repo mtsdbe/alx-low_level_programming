e972e9223e9bdb64351226855fdf1ed5007ad11d
mtsdbe <mtsdbe@gmail.com>
2022-03-15 21:59:39 +0300

    hi

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * Betty check main
+ *0x02-functions_nested_loops
+ **/
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
